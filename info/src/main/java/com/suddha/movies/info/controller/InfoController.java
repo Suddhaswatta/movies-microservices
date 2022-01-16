7a73f14ed81cb1f1a2f279fc62f37d63a05f971b
Suddhaswatta <suddhaswatta.maity1@gmail.com>
2022-01-16 20:13:17 +0530

    Updated Docker Compose , POM,DTO

--- info/src/main/java/com/suddha/movies/info/controller/InfoController.java
+++ info/src/main/java/com/suddha/movies/info/controller/InfoController.java
@@ -4,16 +4,24 @@ import com.suddha.movies.info.domain.Genre;
 import com.suddha.movies.info.domain.MoviesInfo;
 import com.suddha.movies.info.dto.MovieInfoDTO;
 import com.suddha.movies.info.service.InfoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.validation.beanvalidation.CustomValidatorBean;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Valid;
+import javax.validation.Validation;
+import javax.validation.Validator;
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/${api.name}/${api.version}/${api.resource}")
+@Slf4j
 public class InfoController {
 
 
@@ -45,7 +53,12 @@ public class InfoController {
     }
 
     @PostMapping
-    public Mono<MoviesInfo> save(@RequestBody MovieInfoDTO moviesInfo) {
+    public Mono<MoviesInfo> save(@RequestBody @Valid MovieInfoDTO moviesInfo) {
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        Set<ConstraintViolation<MovieInfoDTO>> violations = validator.validate(moviesInfo);
+        for (ConstraintViolation<MovieInfoDTO> violation : violations) {
+            log.error(violation.getMessage());
+        }
         return infoService.save(Mono.just(moviesInfo));
     }
 
