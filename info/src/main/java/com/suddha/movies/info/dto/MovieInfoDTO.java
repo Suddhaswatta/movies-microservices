6a36b08b17bb8274e779f7d51719025addf41538
Suddhaswatta <suddhaswatta.maity1@gmail.com>
2022-01-18 00:40:27 +0530

    Update DTO Constraints

--- info/src/main/java/com/suddha/movies/info/dto/MovieInfoDTO.java
+++ info/src/main/java/com/suddha/movies/info/dto/MovieInfoDTO.java
@@ -3,30 +3,23 @@ package com.suddha.movies.info.dto;
 import com.suddha.movies.info.domain.Genre;
 import lombok.Data;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.util.List;
 
 @Data
 public class MovieInfoDTO {
 
-    @NotNull(message = "Movie ID cannot be null")
-    @NotEmpty(message = "Movie ID cannot be empty")
+    @NotBlank(message = "Movie ID cannot be blank")
+    @Positive
     private String id;
 
-    @NotNull(message = "Movie name cannot be null")
-    @NotEmpty(message = "Movie name cannot be empty")
+    @NotBlank(message = "Movie name cannot be blank")
     @Size(min = 2, max = 24, message = "Movie name must have minimum of 2 characters and maximum of 24 characters")
     private String name;
 
+    private List<@NotEmpty(message = "Movie genre cannot be empty") Genre> genre;
 
-    @NotNull(message = "Movie genre cannot be null")
-    @NotEmpty(message = "Movie genre cannot be empty")
-    private List<Genre> genre;
-
-    @NotNull(message = "Movie release year cannot be null")
+    @NotBlank(message = "Movie release year cannot be blank")
     @Min(value = 1900, message = "Release year must be greater than 1900")
     private Integer releaseYear;
 
